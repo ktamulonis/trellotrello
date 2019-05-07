994df122291d393aeb4cece55b086352a1dcb769
ktamulonis <kurttamulonis@gmail.com>
2019-05-06 22:11:46 -0400

    add's validation spec for board

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Board, type: :model do
   it "has a factory" do
   	expect(create :board).to be_valid
   end
+
+  it "has a user" do 
+    expect(build :board, user:nil).not_to be_valid
+  end
+
 end
