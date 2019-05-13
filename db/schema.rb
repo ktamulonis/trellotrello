cab7d9860a7d5fc767b7858c74160c9d153d6641
ktamulonis <kurttamulonis@gmail.com>
2019-05-12 21:25:40 -0400

    add's positon integer to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_142303) do
+ActiveRecord::Schema.define(version: 2019_05_13_012358) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2019_05_08_142303) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "board_id"
+    t.integer "list_id"
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_cards_on_board_id"
+    t.index ["list_id"], name: "index_cards_on_list_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "identities", force: :cascade do |t|
@@ -42,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_142303) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["board_id"], name: "index_lists_on_board_id"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
